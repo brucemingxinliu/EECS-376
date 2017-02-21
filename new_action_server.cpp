f2dec377d5f4b4b7db9d222c8c6fb3c47a89afe0
brucemingxinliu <mxl592@case.edu>
2017-02-20 22:58:36 -0500

    Create new_action_server.cpp

---
+++ new_action_server.cpp
@@ -0,0 +1,136 @@
+#include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+#include <new_action_service/newAction.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Twist.h>
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <std_msgs/Float64.h>
+
+class newActionServer
+{
+  private:
+  	ros::NodeHandle nh_;
+  	actionlib::SimpleActionServer<new_action_server::newAction> as_;
+  	ros::Publisher new_publisher_object;
+
+  	new_action_server::newGoal goal_;
+  	new_action_server::newFeedback feedback_;
+
+
+
+
+
+const double g_move_speed = 1.0; // set forward speed to this value, e.g. 1m/s
+const double g_spin_speed = 1.0; // set yaw rate to this value, e.g. 1 rad/s
+const double g_sample_dt = 0.01;
+const double g_dist_tol = 0.01; // 1cm
+//global variables, including a publisher object
+geometry_msgs::Twist g_twist_cmd;
+ros::Publisher g_twist_commander; //global publisher object
+geometry_msgs::Pose g_current_pose;
+double sgn(double x);
+double min_spin(double spin_angle);
+double convertPlanarQuat2Phi(geometry_msgs::Quaternion quaternion);
+geometry_msgs::Quaternion convertPlanarPhi2Quaternion(double phi);
+
+void do_halt();
+void do_move(double distance);
+void do_spin(double spin_ang);
+
+//signum function: strip off and return the sign of the argument
+double sgn(double x) { if (x>0.0) {return 1.0; }
+    else if (x<0.0) {return -1.0;}
+    else {return 0.0;}
+}
+
+//a function to consider periodicity and find min delta angle
+double min_spin(double spin_angle) {
+        if (spin_angle>M_PI) {
+            spin_angle -= 2.0*M_PI;}
+        if (spin_angle< -M_PI) {
+            spin_angle += 2.0*M_PI;}
+         return spin_angle;   
+}            
+
+// a useful conversion function: from quaternion to yaw
+double convertPlanarQuat2Phi(geometry_msgs::Quaternion quaternion) {
+    double quat_z = quaternion.z;
+    double quat_w = quaternion.w;
+    double phi = 2.0 * atan2(quat_z, quat_w); // cheap conversion from quaternion to heading for planar motion
+    return phi;
+}
+
+//and the other direction:
+geometry_msgs::Quaternion convertPlanarPhi2Quaternion(double phi) {
+    geometry_msgs::Quaternion quaternion;
+    quaternion.x = 0.0;
+    quaternion.y = 0.0;
+    quaternion.z = sin(phi / 2.0);
+    quaternion.w = cos(phi / 2.0);
+    return quaternion;
+}
+
+// a few action functions:
+//a function to reorient by a specified angle (in radians), then halt
+void do_spin(double spin_ang) {
+    ros::Rate loop_timer(1/g_sample_dt);
+    double timer=0.0;
+    double final_time = fabs(spin_ang)/g_spin_speed;
+    g_twist_cmd.angular.z= sgn(spin_ang)*g_spin_speed;
+    while(timer<final_time) {
+          g_twist_commander.publish(g_twist_cmd);
+          timer+=g_sample_dt;
+          loop_timer.sleep(); 
+          }  
+    do_halt(); 
+} 
+
+//a function to move forward by a specified distance (in meters), then halt
+void do_move(double distance) { // always assumes robot is already oriented properly
+                                // but allow for negative distance to mean move backwards
+    ros::Rate loop_timer(1/g_sample_dt);
+    double timer=0.0;
+    double final_time = fabs(distance)/g_move_speed;
+    g_twist_cmd.angular.z = 0.0; //stop spinning
+    g_twist_cmd.linear.x = sgn(distance)*g_move_speed;
+    while(timer<final_time) {
+          g_twist_commander.publish(g_twist_cmd);
+          timer+=g_sample_dt;
+          loop_timer.sleep(); 
+          }  
+    do_halt();
+}
+
+void do_halt() {
+    ros::Rate loop_timer(1/g_sample_dt);   
+    g_twist_cmd.angular.z= 0.0;
+    g_twist_cmd.linear.x=0.0;
+    for (int i=0;i<10;i++) {
+          g_twist_commander.publish(g_twist_cmd);
+          loop_timer.sleep(); 
+          }   
+}
+
+
+
+//THIS FUNCTION IS NOT FILLED IN: NEED TO COMPUTE HEADING AND TRAVEL DISTANCE TO MOVE
+//FROM START TO GOAL
+void get_yaw_and_dist(geometry_msgs::Pose current_pose, geometry_msgs::Pose goal_pose,double &dist, double &heading) {
+ 
+ dist = 0.0; //FALSE!!
+ if (dist < g_dist_tol) { //too small of a motion, so just set the heading from goal heading
+   heading = convertPlanarQuat2Phi(goal_pose.orientation); 
+ }
+ else {
+    heading = 0.0; //FALSE!!
+ }
+
+}
+void newActionServer::executeCB(const actionlib::SimpleActionServer<new_action_server::newAction>::GoalCOnstPrt& goal)
+{
+   ROS_INFO("Callback activated");
+   
+
+}
