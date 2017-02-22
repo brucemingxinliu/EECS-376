f53c20448e2ebd69099c1a440e08280a8217d674
brucemingxinliu <mxl592@case.edu>
2017-02-21 23:29:39 -0500

    Update new_action_client.cpp

--- new_action_client.cpp
+++ new_action_client.cpp
@@ -7,6 +7,7 @@
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/PoseStamped.h>
 
+
 using namespace std;
 
 bool g_alarm;//declare a global variable of statement
@@ -16,9 +17,21 @@ void alarmCB(const std_msgs::Float64& message_holder)
    g_alarm = true;
 
 }
+
+
+geometry_msgs::PoseStamped g_desired_pose;
+
+void doneCb(const actionlib::SimpleClientGoalState& state,const new_action_server::myResultConstPtr& result)
+{
+
+	result->x_posestamps;
+	result->y_posestamps;
+	result->z_posestamps
+}
+
 int main(int argc, char**argv)
 {
-   ros::init(argc, argv,"new_action_client_node");
+   ros::init(argc, argv,"new_action_client");
    ros::NodeHandle n;
    g_alarm = n.subscribe("lidar_alarm",1,alarmCB);
 
@@ -33,8 +46,32 @@ int main(int argc, char**argv)
    }
    ROS_INFO("Connected to server!");
 
+    geometry_msgs::Quaternion quat;
     geometry_msgs::PoseStamped pose_stamped;
     geometry_msgs::Pose pose;
+
+    new_action_server::new_action_serverGoal;
+    double x;
+    double y;
+    double z;
+    while(ros::ok())
+    {
+    	std::cout<<"Enter a x coordinate for the robot: ";
+    	std::cin>>x;
+    	std::cout<<"Enter a y coordinate for the robot: ";
+    	std::cin>>y;
+    	std::cout<<"Enter a yaw rate for the robot: ";
+    	std::cin>>z;
+
+    
+    goal.x_posestamps = x;
+    goal.y_posestamps = y;
+    goal.z_posestamps = z;
+  
+    ROS_INFO("sending goal: ");
+    action_client.sendGoal(goal,&doneCb);
+    }
+  /*  
     pose.position.x = 1.0; // say desired x-coord is 1
     pose.position.y = 0.0;
     pose.position.z = 0.0; // let's hope so!
@@ -44,216 +81,44 @@ int main(int argc, char**argv)
     pose.orientation.w = 1.0; //sum of squares of all components of unit quaternion is 1
     pose_stamped.pose = pose;
 
+
     quat = convertPlanarPhi2Quaternion(0.0); // get a quaternion corresponding to this heading
     pose_stamped.pose.orientation = quat;   
     pose_stamped.pose.position.x=8.0; // say desired y-coord is 1.0
     pose_stamped.pose.position.y=0.0;
-    goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
+    pose_stamped.pose = pose;
+    newAction.request.nav_path.poses.push_back(pose_stamped);
     
     quat = convertPlanarPhi2Quaternion(0.3);
     pose_stamped.pose.orientation = quat;  
     pose_stamped.pose.position.x=3.0;
     pose_stamped.pose.position.y=3.0;
-     goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
+     pose_stamped.pose = pose;
+     newAction.request.nav_path.poses.push_back(pose_stamped);
 
 
     quat = convertPlanarPhi2Quaternion(1.57);
     pose_stamped.pose.orientation = quat;  
     pose_stamped.pose.position.x=2.0;
     pose_stamped.pose.position.y=3.0;
-    goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
+    pose_stamped.pose = pose;
+    newAction.request.nav_path.poses.push_back(pose_stamped);
 
-    
-    quat = convertPlanarPhi2Quaternion(-0.03);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.x=10.0;
-     goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-    quat = convertPlanarPhi2Quaternion(0.0);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=25.0;
-     goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-
-    quat = convertPlanarPhi2Quaternion(0.001);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=35.0;
-     goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-    quat = convertPlanarPhi2Quaternion(1.3);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=40.0;
-     goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-
-
-    quat = convertPlanarPhi2Quaternion(1.3);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=70.0;
-     goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-
-    quat = convertPlanarPhi2Quaternion(1.3);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=70.0;
-    goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-
-    quat = convertPlanarPhi2Quaternion(1.3);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=70.0;
-     goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-
-    quat = convertPlanarPhi2Quaternion(3.1);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=70.0;
-     goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-
-    quat = convertPlanarPhi2Quaternion(3.1);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=70.0;
-     goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-
-    quat = convertPlanarPhi2Quaternion(3.1);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=70.0;
-     goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-
-    quat = convertPlanarPhi2Quaternion(3.1);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=70.0;
-     goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-    
-    quat = convertPlanarPhi2Quaternion(3.1);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=70.0;
-     goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-
-
-    quat = convertPlanarPhi2Quaternion(2.5);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=70.0;
-     goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-
-    quat = convertPlanarPhi2Quaternion(2.5);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=70.0;
-    goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-
-    quat = convertPlanarPhi2Quaternion(2.5);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=80.0;
-     goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-    
-    quat = convertPlanarPhi2Quaternion(1.5);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=80.0;
-     goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-
-    quat = convertPlanarPhi2Quaternion(1.5);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=80.0;
-     goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-
-    quat = convertPlanarPhi2Quaternion(1.5);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=80.0;
-     goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-
-    quat = convertPlanarPhi2Quaternion(1.5);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=80.0;
-     goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-
-    quat = convertPlanarPhi2Quaternion(1.5);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=80.0;
-    goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-
-    quat = convertPlanarPhi2Quaternion(1.5);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.y=80.0;
-     goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
-
+    */
+   
     if(g_alarm = true){
     ROS_WARN("LIDAR ALARM IS SOUNDED");
+    goal.alarm = 1;
+    action_client.sendGoal(goal);
     
-    quat = convertPlanarPhi2Quaternion(0.3);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.x=3.0;
-    pose_stamped.pose.position.y=3.0;
-    goal.pose_stamped.pose.orientation = pose_stamped.pose.orientation;
-    goal.pose_stamped.pose.position.x =  pose_stamped.pose.position.x;
-    goal.pose_stamped.pose.position.y = pose_stamped.pose.position.y;
-
 
 
 
 
 
 }
+
+
 }
+
 
