3de8af5b9bb015dfa2de49aa9c4ffaa93415c75e
brucemingxinliu <mxl592@case.edu>
2017-02-22 15:30:50 -0500

    Update new_action_client.cpp

--- new_action_client.cpp
+++ new_action_client.cpp
@@ -1,124 +1,111 @@
-#include <ros/ros.h>
+
+
+#include<ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
-#include <new_action_server/newAction.h>
+#include <actionlib/client/terminal_state.h>
+#include <navigator/navigatorAction.h>
+#include <Eigen/Eigen>
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
 #include <std_msgs/Float64.h>
-#include <iostream>
-#include <string>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/PoseStamped.h>
-
-
 using namespace std;
+bool g_alarm;
 
-bool g_alarm;//declare a global variable of statement
+geometry_msgs::PoseStamped g_desired_pose;
+int g_navigator_rtn_code;
 
 void alarmCB(const std_msgs::Float64& message_holder)
 {
-   g_alarm = true;
-
-}
-
-
-geometry_msgs::PoseStamped g_desired_pose;
-
-void doneCb(const actionlib::SimpleClientGoalState& state,const new_action_server::myResultConstPtr& result)
-{
-
-	result->x_posestamps;
-	result->y_posestamps;
-	result->z_posestamps
-}
-
-int main(int argc, char**argv)
-{
-   ros::init(argc, argv,"new_action_client");
-   ros::NodeHandle n;
-   g_alarm = n.subscribe("lidar_alarm",1,alarmCB);
-
-   new_action_server::newGoal goal;
-   actionlib::SimpleActionClient<new_action_server::newAction> action_client("new_action",true);
-   ROS_INFO("Waiting for new_action_server");
-   bool server_exisits = action_client.waitForServer(ros::Duration(5.0));
-   if(!server_exisits)
-   {
-   	  ROS_WARN("Halting...");
-   	  return 0;
-   }
-   ROS_INFO("Connected to server!");
-
-    geometry_msgs::Quaternion quat;
-    geometry_msgs::PoseStamped pose_stamped;
-    geometry_msgs::Pose pose;
-
-    new_action_server::new_action_serverGoal;
-    double x;
-    double y;
-    double z;
-    while(ros::ok())
-    {
-    	std::cout<<"Enter a x coordinate for the robot: ";
-    	std::cin>>x;
-    	std::cout<<"Enter a y coordinate for the robot: ";
-    	std::cin>>y;
-    	std::cout<<"Enter a yaw rate for the robot: ";
-    	std::cin>>z;
-
+    g_alarm = true;
     
-    goal.x_posestamps = x;
-    goal.y_posestamps = y;
-    goal.z_posestamps = z;
-  
-    ROS_INFO("sending goal: ");
-    action_client.sendGoal(goal,&doneCb);
+
+}//lidar alarm void declare...
+
+void navigatorDoneCb(const actionlib::SimpleClientGoalState& state,
+        const navigator::navigatorResultConstPtr& result) {
+    ROS_INFO(" navigatorDoneCb: server responded with state [%s]", state.toString().c_str());
+    g_navigator_rtn_code=result->return_code;
+
+    ROS_INFO("got object code response = %d; ",g_navigator_rtn_code);
+    if (g_navigator_rtn_code==navigator::navigatorResult::DESTINATION_CODE_UNRECOGNIZED) {
+        ROS_WARN("destination code not recognized");
     }
-  /*  
+    else if (g_navigator_rtn_code==navigator::navigatorResult::DESIRED_POSE_ACHIEVED) {
+        ROS_INFO("reached desired location!");
+    }
+    else {
+        ROS_WARN("desired pose not reached!");
+    }
+}
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "example_navigator_action_client"); // name this node 
+    ros::NodeHandle nh; //standard ros node handle    
+    
+    g_alarm = nh.subscribe("lidar_alarm",1,alarmCB);
+    actionlib::SimpleActionClient<navigator::navigatorAction> navigator_ac("navigatorActionServer", true);
+    
+    // attempt to connect to the server:
+    ROS_INFO("waiting for server: ");
+    bool server_exists = false;
+    while ((!server_exists)&&(ros::ok())) {
+        server_exists = navigator_ac.waitForServer(ros::Duration(0.5)); // 
+        ros::spinOnce();
+        ros::Duration(0.5).sleep();
+        ROS_INFO("retrying...");
+    }
+    ROS_INFO("connected to navigator action server"); // if here, then we connected to the server; 
+     
+    navigator::navigatorGoal navigation_goal;
+
+    navigation_goal.location_code=navigator::navigatorGoal::HOME;
+    geometry_msgs::Pose pose;
     pose.position.x = 1.0; // say desired x-coord is 1
     pose.position.y = 0.0;
     pose.position.z = 0.0; // let's hope so!
     pose.orientation.x = 0.0; //always, for motion in horizontal plane
     pose.orientation.y = 0.0; // ditto
     pose.orientation.z = 0.0; // implies oriented at yaw=0, i.e. along x axis
-    pose.orientation.w = 1.0; //sum of squares of all components of unit quaternion is 1
-    pose_stamped.pose = pose;
-
-
-    quat = convertPlanarPhi2Quaternion(0.0); // get a quaternion corresponding to this heading
-    pose_stamped.pose.orientation = quat;   
-    pose_stamped.pose.position.x=8.0; // say desired y-coord is 1.0
-    pose_stamped.pose.position.y=0.0;
-    pose_stamped.pose = pose;
-    newAction.request.nav_path.poses.push_back(pose_stamped);
+    pose.orientation.w = 1.0;
     
-    quat = convertPlanarPhi2Quaternion(0.3);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.x=3.0;
-    pose_stamped.pose.position.y=3.0;
-     pose_stamped.pose = pose;
-     newAction.request.nav_path.poses.push_back(pose_stamped);
+  
+    pose.position.x=8.0; // say desired y-coord is 1.0
+    pose.position.y=0.0;
+    
+    pose.position.x=6.0; // say desired y-coord is 1.0
+    pose.position.y=2.0;
+
+    pose.position.x=0.0; // say desired y-coord is 1.0
+    pose.position.y=0.0;
 
 
-    quat = convertPlanarPhi2Quaternion(1.57);
-    pose_stamped.pose.orientation = quat;  
-    pose_stamped.pose.position.x=2.0;
-    pose_stamped.pose.position.y=3.0;
-    pose_stamped.pose = pose;
-    newAction.request.nav_path.poses.push_back(pose_stamped);
+    g_desired_pose.pose = pose;
 
-    */
-   
-    if(g_alarm = true){
+    navigation_goal.desired_pose = g_desired_pose;
+    
+    ROS_INFO("sending goal: ");
+        navigator_ac.sendGoal(navigation_goal,&navigatorDoneCb); // we could also name additional callback functions here, if desired
+
+        
+        bool finished_before_timeout = navigator_ac.waitForResult(ros::Duration(30.0));
+        //bool finished_before_timeout = action_client.waitForResult(); // wait forever...
+        if (!finished_before_timeout) {
+            ROS_WARN("giving up waiting on result ");
+            return 1;
+        }
+        if(g_alarm = true){
     ROS_WARN("LIDAR ALARM IS SOUNDED");
-    goal.alarm = 1;
-    action_client.sendGoal(goal);
-    
-
-
-
-
-
-}
-
+    ros::Duration(5.0);
+    if(g_alarm = true){
+       return 1;
+    }
+    else{
+    return 0;
 
 }
+}
 
-
+    return 0;
+}
