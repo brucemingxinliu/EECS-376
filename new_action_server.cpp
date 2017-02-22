793515450d14b019ea1b88af63d3eba08d71af14
brucemingxinliu <mxl592@case.edu>
2017-02-22 17:36:24 -0500

    Update new_action_server.cpp

--- new_action_server.cpp
+++ new_action_server.cpp
@@ -1,28 +1,62 @@
-#include <ros/ros.h>
+
+
+
+//so far, the lidar alarm topic is subscribed. However no poses have been given in the client, and still needs to figure out how to send poses via action to the action server. 
+#include<ros/ros.h>
 #include <actionlib/server/simple_action_server.h>
-#include <new_action_service/newAction.h>
+#include<navigator/navigatorAction.h>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Twist.h>
 #include <iostream>
 #include <string>
 #include <math.h>
-#include <std_msgs/Float64.h>
-#include <nav_msgs/Path.h>
 
-class newActionServer
-{
-  private:
-  	ros::NodeHandle nh_;
-  	actionlib::SimpleActionServer<new_action_server::newAction> as_;
-  	ros::Publisher new_publisher_object;
+class Navigator {
+private:
 
-  	new_action_server::newGoal goal_;
-  	new_action_server::newFeedback feedback_;
+    ros::NodeHandle nh_;  // we'll need a node handle; get one upon instantiation
+    actionlib::SimpleActionServer<navigator::navigatorAction> navigator_as_;
+    
+    // here are some message types to communicate with our client(s)
+    navigator::navigatorGoal goal_; // goal message, received from client
+    navigator::navigatorResult result_; // put results here, to be sent back to the client when done w/ goal
+    navigator::navigatorFeedback feedback_; // not used in this example; 
+    // would need to use: as_.publishFeedback(feedback_); to send incremental feedback to the client
+
+    int navigate_home();
+    int navigate_to_table();
+    int navigate_to_pose(geometry_msgs::PoseStamped goal_pose);
+    
+public:
+    Navigator(); //define the body of the constructor outside of class definition
+
+    ~Navigator(void) {
+    }
+    // Action Interface
+    void executeCB(const actionlib::SimpleActionServer<navigator::navigatorAction>::GoalConstPtr& goal);
+};
 
 
 
+Navigator::Navigator() :
+   navigator_as_(nh_, "navigatorActionServer", boost::bind(&Navigator::executeCB, this, _1),false) {
+    ROS_INFO("in constructor of navigator...");
+    // do any other desired initializations here...specific to your implementation
 
+    navigator_as_.start(); //start the server running
+}
 
+//specialized function: DUMMY...JUST RETURN SUCCESS...fix this
+//this SHOULD do the hard work of navigating to HOME
+int Navigator::navigate_home() { 
+    return navigator::navigatorResult::DESIRED_POSE_ACHIEVED; //just say we were successful
+} 
+int Navigator::navigate_to_table() { 
+    return navigator::navigatorResult::DESIRED_POSE_ACHIEVED; //just say we were successful
+}
+int Navigator::navigate_to_pose(geometry_msgs::PoseStamped goal_pose) {
+    return navigator::navigatorResult::DESIRED_POSE_ACHIEVED;
+}
 const double g_move_speed = 1.0; // set forward speed to this value, e.g. 1m/s
 const double g_spin_speed = 1.0; // set yaw rate to this value, e.g. 1 rad/s
 const double g_sample_dt = 0.01;
@@ -30,7 +64,10 @@ const double g_dist_tol = 0.01; // 1cm
 //global variables, including a publisher object
 geometry_msgs::Twist g_twist_cmd;
 ros::Publisher g_twist_commander; //global publisher object
-geometry_msgs::Pose g_current_pose;
+geometry_msgs::Pose g_current_pose; // not really true--should get this from odom 
+
+
+// here are a few useful utility functions:
 double sgn(double x);
 double min_spin(double spin_angle);
 double convertPlanarQuat2Phi(geometry_msgs::Quaternion quaternion);
@@ -40,13 +77,10 @@ void do_halt();
 void do_move(double distance);
 void do_spin(double spin_ang);
 
-//signum function: strip off and return the sign of the argument
 double sgn(double x) { if (x>0.0) {return 1.0; }
     else if (x<0.0) {return -1.0;}
     else {return 0.0;}
 }
-
-//a function to consider periodicity and find min delta angle
 double min_spin(double spin_angle) {
         if (spin_angle>M_PI) {
             spin_angle -= 2.0*M_PI;}
@@ -73,8 +107,6 @@ geometry_msgs::Quaternion convertPlanarPhi2Quaternion(double phi) {
     return quaternion;
 }
 
-// a few action functions:
-//a function to reorient by a specified angle (in radians), then halt
 void do_spin(double spin_ang) {
     ros::Rate loop_timer(1/g_sample_dt);
     double timer=0.0;
@@ -129,84 +161,98 @@ void get_yaw_and_dist(geometry_msgs::Pose current_pose, geometry_msgs::Pose goal
  }
 
 }
-void newActionServer::executeCB(const actionlib::SimpleActionServer<new_action_server::newAction>::GoalCOnstPrt& goal)
-{
-   ROS_INFO("Callback activated");
-   double yaw_desired, yaw_current, travel_distance, spin_angle;
-   geometry_msgs::Pose pose_desired;
-   
-   while(ros::ok())
-   {
+void Navigator::executeCB(const actionlib::SimpleActionServer<navigator::navigatorAction>::GoalConstPtr& goal) {
+    ROS_INFO("callback activated");
+    double yaw_desired, yaw_current, travel_distance, spin_angle;
+    geometry_msgs::Pose desired_pose;
+    geometry_msgs::PoseStamped pose_id;
+    pose_id = goal->desired_pose;
+    
+
+   // int destination_id = goal->desired.pose;
+   /* geometry_msgs::PoseStamped destination_pose;
+    int navigation_status;
+    int n_pts = destination_id.size();
+    for(int i=1;n_pts;i++){
 
 
-   double alarm = goal->alarm;
-   double x = goal->x_posestamps;
-   double y = goal->y_posestamps;
-   double z = goal->z_posestamps;
-   do_move(x);
-   do_spin(z);
-   do_move(y);
-   if(alarm ==1)
-   {
-   	do_halt();
-   }
 
-   /* for (int i=0;i<npts;i++) { //visit each subgoal
-        // odd notation: drill down, access vector element, drill some more to get pose
-        pose_desired = request.nav_path.poses[i].pose; //get next pose from vector of poses
-
-        yaw_desired = convertPlanarQuat2Phi(pose_desired.orientation); //from i'th desired pose
-        
-        ROS_INFO("pose %d: desired yaw = %f",i,yaw_desired);        
-        yaw_current = convertPlanarQuat2Phi(g_current_pose.orientation); //our current yaw--should use a sensor
-        spin_angle = yaw_desired - yaw_current; // spin this much
-        spin_angle = min_spin(spin_angle);// but what if this angle is > pi?  then go the other way
-        do_spin(spin_angle); // carry out this incremental action
-        // we will just assume that this action was successful--really should have sensor feedback here
-        g_current_pose.orientation = pose_desired.orientation; // assumes got to desired orientation precisely
-        
-        //FIX THE NEXT LINE, BASED ON get_yaw_and_dist()
-        do_move(3.0);  // move forward 1m...just for illustration; SHOULD compute this from subgoal pose
-        }
-*/
-  return true;
 }
+/*
+    /*if (destination_id==navigator::navigatorGoal::COORDS) {
+        destination_pose=goal->desired_pose;
+    }
+    
+    switch(destination_id) {
+        case navigator::navigatorGoal::HOME: 
+              //specialized function to navigate to pre-defined HOME coords
+               navigation_status = navigate_home(); 
+               if (navigation_status==navigator::navigatorResult::DESIRED_POSE_ACHIEVED) {
+                   ROS_INFO("reached home");
+                   result_.return_code = navigator::navigatorResult::DESIRED_POSE_ACHIEVED;
+                   navigator_as_.setSucceeded(result_);
+               }
+               else {
+                   ROS_WARN("could not navigate home!");
+                   navigator_as_.setAborted(result_);
+               }
+               break;
+        case navigator::navigatorGoal::TABLE: 
+              //specialized function to navigate to pre-defined TABLE coords
+               navigation_status = navigate_to_table(); 
+               if (navigation_status==navigator::navigatorResult::DESIRED_POSE_ACHIEVED) {
+                   ROS_INFO("reached table");
+                   result_.return_code = navigator::navigatorResult::DESIRED_POSE_ACHIEVED;
+                   navigator_as_.setSucceeded(result_);
+               }
+               else {
+                   ROS_WARN("could not navigate to table!");
+                   navigator_as_.setAborted(result_);
+               }
+               break;
+        case navigator::navigatorGoal::COORDS: 
+              //more general function to navigate to specified pose:
+              destination_pose=goal->desired_pose;
+               navigation_status = navigate_to_pose(destination_pose); 
+               if (navigation_status==navigator::navigatorResult::DESIRED_POSE_ACHIEVED) {
+                   ROS_INFO("reached desired pose");
+                   result_.return_code = navigator::navigatorResult::DESIRED_POSE_ACHIEVED;
+                   navigator_as_.setSucceeded(result_);
+               }
+               else {
+                   ROS_WARN("could not navigate to desired pose!");
+                   navigator_as_.setAborted(result_);
+               }
+               break;               
+               
+        default:
+             ROS_WARN("this location ID is not implemented");
+             result_.return_code = navigator::navigatorResult::DESTINATION_CODE_UNRECOGNIZED; 
+             navigator_as_.setAborted(result_);
+            }
+  */
 }
-//how to call the points to callback function...
 
 
-/*void do_inits(ros::NodeHandle &n) {
-  //initialize components of the twist command global variable
-    g_twist_cmd.linear.x=0.0;
-    g_twist_cmd.linear.y=0.0;    
-    g_twist_cmd.linear.z=0.0;
-    g_twist_cmd.angular.x=0.0;
-    g_twist_cmd.angular.y=0.0;
-    g_twist_cmd.angular.z=0.0;  
-    
-    //define initial position to be 0
-    g_current_pose.position.x = 0.0;
-    g_current_pose.position.y = 0.0;
-    g_current_pose.position.z = 0.0;
-    
-    // define initial heading to be "0"
-    g_current_pose.orientation.x = 0.0;
-    g_current_pose.orientation.y = 0.0;
-    g_current_pose.orientation.z = 0.0;
-    g_current_pose.orientation.w = 1.0;
-    
-    // we declared g_twist_commander as global, but never set it up; do that now that we have a node handle
-    g_twist_commander = n.advertise<geometry_msgs::Twist>("/robot0/cmd_vel", 1);    
-}*/
+ //ros::NodeHandle n;
+ //ros::Publisher g_twist_commander = n.advertise<geometry_msgs::Twist>("/robot0/cmd_vel", 1); 
+//the topic is published to robot0...
 
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "new_action_server");
-  ros::NodeHandle n;
 
-  newActionServer actionserver;
-  ROS_INFO("Ready to accept paths.");
-  ros::spin(); //callbacks do all the work now
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "navigation_action_server"); // name this node 
 
-  return 0;
+    ROS_INFO("instantiating the navigation action server: ");
+    ros::NodeHandle n;
+    Navigator navigator_as; // create an instance of the class "ObjectFinder"
+
+    ROS_INFO("going into spin");
+    // from here, all the work is done in the action server, with the interesting stuff done within "executeCB()"
+    // you will see 5 new topics under example_action: cancel, feedback, goal, result, status
+    while (ros::ok()) {
+        ros::spinOnce(); //normally, can simply do: ros::spin();  
+        // for debug, induce a halt if we ever get our client/server communications out of sync
+    }
+
+    return 0;
 }
